cb12c2ea2586b891ee22b32e6ab46c5fdbe6de02
nanspro <rahulbishnoi3636@gmail.com>
2019-09-28 08:22:55 +0530

    New Entities

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,9 +1,10 @@
-import { BigInt, Bytes, log, JSONValue } from "@graphprotocol/graph-ts"
+import { BigInt, Bytes, log, JSONValue, DataSourceTemplate } from "@graphprotocol/graph-ts"
 import {
   Contract,
   ArticlesCheckpointed,
+  ArticleHash,
 } from "../generated/Contract/Contract"
-import { Checkpoint } from "../generated/schema"
+import { Checkpoint, Article, Attribute } from "../generated/schema"
 import { loadFromIpfs } from "./ipfs"
 import { TransactionInfo, State } from "./transaction";
 import { asString } from "./util";
@@ -27,58 +28,75 @@ export function handleArticlesCheckpointed(event: ArticlesCheckpointed): void {
   tx.state.ipfsReqs = 0
   
   let ipfsData = loadFromIpfs(ipfsHash, tx)
-  var contentHash: string[]
-  let length: number
 
   log.info("hey {}", [ipfsHash])
   if (ipfsData !== null){
     log.info("Inside IPFS Hash {}", [ipfsHash])
     let articles = ipfsData.get('articleLeaves').toArray()
     
-    for (let i = 0; i < articles.length; i++) {
-      log.info("Count is: {}", [i.toString()])
-      let article = articles[i].toObject()
-      let articleHash = article.get('contentHash')
-      if (articleHash !== null) {
-        log.info("Content Hash: {} {}", [i.toString(), articleHash.toString()])
-        length = contentHash.push(articleHash.toString())
-      }
+    let article = articles[0].toObject()
+    let articleHash = article.get('contentHash')
+    let entity2 = new Article(event.transaction.hash.toHex())
+        
+    if (articleHash !== null) {
+        let attribute = new Attribute(event.transaction.hash.toHex())
+
+        entity2.ipfsHash = articleHash.toString()
+        log.info("Content Hash: {}", [articleHash.toString()])
+
+        let data = loadFromIpfs(articleHash.toString(), tx)
+        if (data !== null) {
+          log.info("Inside Content Hash {}", [articleHash.toString()])
+          let title = data.get('title')
+          let content = data.get('content')
+          let tagArray = data.get('tags').toArray()
+          let tag = tagArray[0].toString()
+          let author = data.get('author').toString()
+
+          // var tags: string
+          // tagArray.forEach(function(item) {
+            // tags = tags + item.toString() + ' '
+          // })
+          author = '0x' + author
+          let timestamp = data.get('timestamp')
+          log.info("Content Title is: {}", [title.toString()])
+          // log.info("Content is: {}", [content.toString()])
+          log.info("Author is: {}", [author.toString()])
+          log.info("Created at: {}", [timestamp.toBigInt().toString()])
+          log.info("Tags array: {}", [tagArray[0].toString()])
+          log.info("One of the tags is: {}", [tag])
+
+          entity2.content = content.toString()
+          entity2.title = title.toString()
+          entity2.author = author.toString()
+          entity2.createdAt = timestamp.toBigInt()
+          // entity2.tags = tags
+
+          let attributes = data.get('attributes').toObject()
+          let originName = attributes.get('origin_name')
+          let originUrl = attributes.get('origin_url')
+          let background = attributes.get('background')
+          if (originName !== null){
+            attribute.originName = originName.toString()
+            log.info("Origin Name: {}", [originName.toString()])
+          }
+          if (originUrl !== null){
+            attribute.originUrl = originUrl.toString()
+            log.info("Origin Url: {}", [originUrl.toString()])
+          }
+          if (background !== null){
+            attribute.background = background.toString()
+            log.info("Background: {}", [background.toString()])
+          }
+          
+          attribute.save()
+
+          entity2.attributes = attribute.id
+          
+          entity2.save()
+        }
     }
-    log.info("Length {}", [length.toString()])
-    log.info("Bye {}", [ipfsHash])
-    // entity.contentHash = contentHash
-
-    // for (let i = 0; i < contentHash.length; i++) {
-    //   let data = loadFromIpfs(contentHash[i], tx)
-    //   if (data !== null) {
-    //     log.info("Inside Content Hash {}", [contentHash[i]])
-    //     let title = data.get('title')
-    //     let content = data.get('content')
-    //     let tagArray = data.get('tags').toArray()
-    //     let tag = tagArray[0].toString()
-    //     let author = data.get('author').toString()
-    //     author = '0x' + author
-    //     let timestamp = data.get('timestamp')
-    //     log.info("Content Title is: {}", [title.toString()])
-    //     // log.info("Content is: {}", [content.toString()])
-    //     log.debug("Author is: {}", [author.toString()])
-    //     log.debug("Created at: {}", [timestamp.toBigInt().toString()])
-    //     log.debug("Tags array: {}", [tagArray[0].toString()])
-    //     log.debug("One of the tags is: {}", [tag])
-    //   }
-    // }
-
-      // let attributes = contentData.get('attributes').toObject()
-      // let origin_name = attributes.get('origin_name') 
-      // let origin_url = attributes.get('origin_url')
-      // let background = attributes.get('background')
-      
-      // log.debug("Origin Name: {}", [origin_name.toString()])
-      // log.debug("Origin Url: {}", [origin_url.toString()])
-      // log.debug("Background: {}", [background.toString()])
-
   }
 
-  // entity.ipfsContent = ipfsContent
   entity.save()
 }
